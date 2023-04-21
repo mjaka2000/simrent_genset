beba8877490e4b06f9bf9a5b445fed73f55b8e9a
Muhammad Jaka Permana Putra Agus <jakamuhammad28@gmail.com>
2023-04-21 11:34:28 +0800

    ubah view report

--- application/views/admin/report/laporan.php
+++ application/views/admin/report/laporan.php
@@ -12,7 +12,7 @@
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="<?= base_url('admin'); ?>"><i class="fas fa-home"></i></a></li>
                         <li class="breadcrumb-item active">Report</li>
                     </ol>
                 </div><!-- /.col -->
