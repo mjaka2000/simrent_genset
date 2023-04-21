88d4e272f50e0a0542469119ef0c25de4097c906
Muhammad Jaka Permana Putra Agus <jakamuhammad28@gmail.com>
2023-04-21 11:25:55 +0800

    ubah view report

---
+++ application/views/admin/report/laporan.php
@@ -0,0 +1,93 @@
+<?php $this->load->view('template/head'); ?>
+<?php $this->load->view('admin/template/nav'); ?>
+<?php $this->load->view('admin/template/sidebar'); ?>
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Report</small></h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Report</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div id="loading">
+            <img src="<?= base_url(); ?>assets/style/loading.gif" alt="loading" width="50%">
+        </div>
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <h2 align="center">Selamat Datang, <strong><?= $this->session->userdata('name') ?></strong> sebagai Administrator!</h2>
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <i class="fa fa-clipboard"></i>
+                            Laporan Data
+                        </div>
+                        <div class="card-body">
+                            <table id="tablepakai" class="table table-bordered" style="width:100%">
+                                <tr>
+                                    <form action="<?= base_url(); ?>admin/#" method="post" role="form" target="_blank">
+                                        <th style="vertical-align: middle">Laporan #</th>
+                                        <td style="vertical-align: middle;">
+                                            <div class="row">
+                                                <div class="col-lg-8">
+                                                    <div class="row">
+                                                        <div class="col-md-4">
+                                                            <input type="date" name="tgl_awal" class="form-control" id="tgl_update" placeholder="Tanggal Update" required value="<?= date('Y-m-d'); ?>">
+                                                        </div><span>&nbsp;s/d&nbsp;</span>
+                                                        <div class="col-md-4">
+                                                            <input type="date" name="tgl_akhir" class="form-control" id="tgl_update" placeholder="Tanggal Update" required value="<?= date('Y-m-d'); ?>">
+                                                        </div>
+                                                        <div class="col-md-2">
+                                                            <button type="submit" class="btn btn-info"><i class="fas fa-print mr-2"></i> Cetak</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </form>
+                                </tr>
+                            </table>
+                            <!-- </div> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+</div>
+<?php $this->load->view('template/footer'); ?>
+
+<?php $this->load->view('admin/template/script') ?>
+<script>
+    //* Script untuk menampilkan loading
+    document.onreadystatechange = function() {
+        if (document.readyState !== "complete") {
+            document.querySelector(
+                "body").style.visibility = "hidden";
+            document.querySelector(
+                "#loading").style.visibility = "visible";
+        } else {
+            document.querySelector(
+                "#loading").style.display = "none";
+            document.querySelector(
+                "body").style.visibility = "visible";
+        }
+    };
+</script>
+</body>
+
+</html>
