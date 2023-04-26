bdfd272bf12168920602f8447eac9aabac8e11d0
Muhammad Jaka Permana Putra Agus <jakamuhammad28@gmail.com>
2023-04-26 11:41:53 +0800

    db upload

--- assets/backup_db/db_rent_genset.sql
+++ assets/backup_db/db_rent_genset.sql
@@ -11,7 +11,7 @@ CREATE TABLE `tb_avatar` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
-INSERT INTO `tb_avatar` (`id`, `username_user`, `nama_file`) VALUES (1, 'admin', 'jaka.jpg');
+INSERT INTO `tb_avatar` (`id`, `username_user`, `nama_file`) VALUES (1, 'admin', 'jaka1.jpg');
 INSERT INTO `tb_avatar` (`id`, `username_user`, `nama_file`) VALUES (12, 'jakaja', 'nopic.png');
 INSERT INTO `tb_avatar` (`id`, `username_user`, `nama_file`) VALUES (13, 'bos', 'nopic.png');
 INSERT INTO `tb_avatar` (`id`, `username_user`, `nama_file`) VALUES (14, 'aril', 'nopic.png');
@@ -69,14 +69,17 @@ DROP TABLE IF EXISTS `tb_operator`;
 
 CREATE TABLE `tb_operator` (
   `id_operator` int(11) NOT NULL AUTO_INCREMENT,
-  `nama` varchar(50) NOT NULL,
-  `alamat` varchar(50) NOT NULL,
-  `no_hp` varchar(50) NOT NULL,
+  `nama_op` varchar(50) NOT NULL,
+  `alamat_op` varchar(50) NOT NULL,
+  `nohp_op` varchar(50) NOT NULL,
   `status_op` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_operator`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
-INSERT INTO `tb_operator` (`id_operator`, `nama`, `alamat`, `no_hp`, `status_op`) VALUES (1, 'Jaka Ja', 'Jl. sungai jingah', '0895619019104', 1);
+INSERT INTO `tb_operator` (`id_operator`, `nama_op`, `alamat_op`, `nohp_op`, `status_op`) VALUES (1, 'Jaka Ja', 'Jl. sungai jingah', '0895619019104', 1);
+INSERT INTO `tb_operator` (`id_operator`, `nama_op`, `alamat_op`, `nohp_op`, `status_op`) VALUES (2, 'adi', 'jl sukamara', '0878907678956', 1);
+INSERT INTO `tb_operator` (`id_operator`, `nama_op`, `alamat_op`, `nohp_op`, `status_op`) VALUES (3, 'ijum', 'jl pulau laut', '0897819271234', 1);
+INSERT INTO `tb_operator` (`id_operator`, `nama_op`, `alamat_op`, `nohp_op`, `status_op`) VALUES (4, 'wanda', 'sungai miai', '0897618391837', 1);
 
 
 #
@@ -87,17 +90,18 @@ DROP TABLE IF EXISTS `tb_pelanggan`;
 
 CREATE TABLE `tb_pelanggan` (
   `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT,
-  `nama` varchar(50) NOT NULL,
-  `alamat` varchar(50) NOT NULL,
-  `no_hp` varchar(20) NOT NULL,
-  `jenis_kelamin` varchar(20) NOT NULL,
-  `nama_perusahaan` varchar(50) NOT NULL,
-  `tanggal_update` date NOT NULL,
-  `ket_plg` int(11) DEFAULT NULL,
+  `nama_plg` varchar(50) NOT NULL,
+  `alamat_plg` varchar(50) NOT NULL,
+  `nohp_plg` varchar(20) NOT NULL,
+  `jk_plg` varchar(20) NOT NULL,
+  `namaperusahaan_plg` varchar(50) NOT NULL,
+  `tglupdate_plg` date NOT NULL,
+  `ket_plg` int(11) DEFAULT 1,
   PRIMARY KEY (`id_pelanggan`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
-INSERT INTO `tb_pelanggan` (`id_pelanggan`, `nama`, `alamat`, `no_hp`, `jenis_kelamin`, `nama_perusahaan`, `tanggal_update`, `ket_plg`) VALUES (4, 'anonim', 'Jl. SungaAndaii', '0895619213134', 'Laki-Laki', 'PT Rahmat', '2023-04-11', 1);
+INSERT INTO `tb_pelanggan` (`id_pelanggan`, `nama_plg`, `alamat_plg`, `nohp_plg`, `jk_plg`, `namaperusahaan_plg`, `tglupdate_plg`, `ket_plg`) VALUES (4, 'anonim', 'Jl. SungaAndaii', '0895619213134', 'Laki-Laki', 'PT Rahmat', '2023-04-11', 1);
+INSERT INTO `tb_pelanggan` (`id_pelanggan`, `nama_plg`, `alamat_plg`, `nohp_plg`, `jk_plg`, `namaperusahaan_plg`, `tglupdate_plg`, `ket_plg`) VALUES (5, 'iwan f', 'jl kp melayu', '0878123121234', 'Laki-Laki', 'pt rahmat', '2023-04-16', 1);
 
 
 #
@@ -108,16 +112,17 @@ DROP TABLE IF EXISTS `tb_pelanggan_blacklist`;
 
 CREATE TABLE `tb_pelanggan_blacklist` (
   `id_plg_blacklist` int(11) NOT NULL AUTO_INCREMENT,
-  `nama` varchar(50) NOT NULL,
-  `alamat` varchar(50) NOT NULL,
-  `no_hp` varchar(20) NOT NULL,
-  `jenis_kelamin` varchar(20) NOT NULL,
-  `nama_perusahaan` varchar(50) NOT NULL,
-  `tanggal_update` date NOT NULL,
+  `nama_plg_blk` varchar(50) NOT NULL,
+  `alamat_plg_blk` varchar(50) NOT NULL,
+  `nohp_plg_blk` varchar(20) NOT NULL,
+  `jk_plg_blk` varchar(20) NOT NULL,
+  `namaperusahaan_plg_blk` varchar(50) NOT NULL,
+  `tglupdate_plg_blk` date NOT NULL,
   PRIMARY KEY (`id_plg_blacklist`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
-INSERT INTO `tb_pelanggan_blacklist` (`id_plg_blacklist`, `nama`, `alamat`, `no_hp`, `jenis_kelamin`, `nama_perusahaan`, `tanggal_update`) VALUES (2, 'Engkoh', 'Sungai Jingah', '089561921342', 'Laki-Laki', 'PT RTR', '2023-04-11');
+INSERT INTO `tb_pelanggan_blacklist` (`id_plg_blacklist`, `nama_plg_blk`, `alamat_plg_blk`, `nohp_plg_blk`, `jk_plg_blk`, `namaperusahaan_plg_blk`, `tglupdate_plg_blk`) VALUES (2, 'Engkoh', 'Sungai Jingah', '089561921342', 'Laki-Laki', 'PT RTR', '2023-04-11');
+INSERT INTO `tb_pelanggan_blacklist` (`id_plg_blacklist`, `nama_plg_blk`, `alamat_plg_blk`, `nohp_plg_blk`, `jk_plg_blk`, `namaperusahaan_plg_blk`, `tglupdate_plg_blk`) VALUES (3, 'ami', 'jl. sjingah', '0821312341087', 'Perempuan', 'pt amanah', '2023-04-18');
 
 
 #
@@ -143,7 +148,6 @@ CREATE TABLE `tb_serv_genset` (
 
 INSERT INTO `tb_serv_genset` (`id_perbaikan_gst`, `id_genset`, `id_sparepart`, `jenis_perbaikan`, `tgl_perbaikan`, `ket_perbaikan`, `biaya_perbaikan`) VALUES (1, 2, 1, 'ganti filter solar', '2023-04-12', 'Selesai Diperbaiki', '0');
 INSERT INTO `tb_serv_genset` (`id_perbaikan_gst`, `id_genset`, `id_sparepart`, `jenis_perbaikan`, `tgl_perbaikan`, `ket_perbaikan`, `biaya_perbaikan`) VALUES (2, 3, 1, 'Ganti Oli', '2023-04-11', 'Selesai Diperbaiki', '250000');
-INSERT INTO `tb_serv_genset` (`id_perbaikan_gst`, `id_genset`, `id_sparepart`, `jenis_perbaikan`, `tgl_perbaikan`, `ket_perbaikan`, `biaya_perbaikan`) VALUES (3, 2, 2, 'oli', '2023-04-10', 'Selesai Diperbaiki', '100000');
 
 
 #
@@ -234,9 +238,9 @@ CREATE TABLE `tb_user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
-INSERT INTO `tb_user` (`id`, `username`, `nama`, `password`, `role`, `last_login`) VALUES (1, 'admin', 'admin1', '$2y$10$/TIhUCTyJV0mBGwMcbDmSOgG7KgFgnucybRnPq1L.xhA3wxfqxfgO', 0, '17-04-2023 19:30');
+INSERT INTO `tb_user` (`id`, `username`, `nama`, `password`, `role`, `last_login`) VALUES (1, 'admin', 'admin1', '$2y$10$eZ1p2/8Ne1va1k5JQDqz2eJQ68mEDCV/LPYrIIDa0GtORa9KGkez2', 0, '26-04-2023 11:10');
 INSERT INTO `tb_user` (`id`, `username`, `nama`, `password`, `role`, `last_login`) VALUES (31, 'jakaja', 'Jaka', '$2y$10$BKPTzEWnptxgS63pLJh8UeCEG2POxhVqpL297bC3w6fEeOWF7QPw6', 0, '13-04-2023 9:56');
-INSERT INTO `tb_user` (`id`, `username`, `nama`, `password`, `role`, `last_login`) VALUES (32, 'bos', 'Bos Jaka', '$2y$10$R4e0tMDfAU.8nz41SxIIhOQ1J5.itOq.sbA8YEAUzKJOSTVUJnV/m', 1, '13-04-2023 9:57');
-INSERT INTO `tb_user` (`id`, `username`, `nama`, `password`, `role`, `last_login`) VALUES (33, 'aril', 'Teknik', '$2y$10$bX/22YuDFyiEtVzcX17ofujConoU4Rgl/KmrFBzKqU2E7RaAqgLIO', 2, '13-04-2023 9:58');
+INSERT INTO `tb_user` (`id`, `username`, `nama`, `password`, `role`, `last_login`) VALUES (32, 'bos', 'Bos Jaka', '$2y$10$R4e0tMDfAU.8nz41SxIIhOQ1J5.itOq.sbA8YEAUzKJOSTVUJnV/m', 1, '25-04-2023 21:18');
+INSERT INTO `tb_user` (`id`, `username`, `nama`, `password`, `role`, `last_login`) VALUES (33, 'aril', 'Teknik', '$2y$10$bX/22YuDFyiEtVzcX17ofujConoU4Rgl/KmrFBzKqU2E7RaAqgLIO', 2, '26-04-2023 11:08');
 
 
