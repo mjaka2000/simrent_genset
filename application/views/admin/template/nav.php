de210217fc8f9285fccec90dada2982e059e6c4e
Muhammad Jaka Permana Putra Agus <jakamuhammad28@gmail.com>
2023-05-30 10:38:11 +0800

    Admin : change view  nav admin user button

--- application/views/admin/template/nav.php
+++ application/views/admin/template/nav.php
@@ -17,43 +17,43 @@
         </li> -->
 
     </ul>
-    <div class="navbar-custom-menu">
-        <ul class="nav navbar-nav">
-            <!-- Messages: style can be found in dropdown.less-->
+    <ul class="nav navbar-nav">
+        <!-- Messages: style can be found in dropdown.less-->
 
-            <!-- User Account: style can be found in dropdown.less -->
-            <li class="dropdown user user-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        <!-- User Account: style can be found in dropdown.less -->
+        <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <?php foreach ($avatar as $a) { ?>
+                    <img src="<?= base_url('assets/upload/user/' . $a->nama_file); ?>" class="user-image" alt="User Image">
+                <?php } ?>
+                <span class="hidden-xs" style="color: white;"><?= $this->session->userdata('name') ?></span>
+            </a>
+            <ul class="dropdown-menu">
+                <!-- User image -->
+                <li class="user-header">
                     <?php foreach ($avatar as $a) { ?>
-                        <img src="<?= base_url('assets/upload/user/' . $a->nama_file); ?>" class="user-image" alt="User Image">
+                        <img src="<?= base_url('assets/upload/user/' . $a->nama_file); ?>" class="profile-user-img img-responsive img-circle" alt="User Image">
                     <?php } ?>
-                    <span class="hidden-xs" style="color: white;"><?= $this->session->userdata('name') ?></span>
-                </a>
-                <ul class="dropdown-menu">
-                    <!-- User image -->
-                    <li class="user-header">
-                        <?php foreach ($avatar as $a) { ?>
-                            <img src="<?= base_url('assets/upload/user/' . $a->nama_file); ?>" class="profile-user-img img-responsive img-circle" alt="User Image">
-                        <?php } ?>
 
-                        <p>
-                            <strong><?= $this->session->userdata('name') ?></strong> as Administrator <br>
-                            <small>Last Login : <?= $this->session->userdata('last_login') ?></small><br>
-                        </p>
-                    </li>
-                    <!-- Menu Body -->
+                    <p>
+                        <strong><?= $this->session->userdata('name') ?></strong> as Administrator <br>
+                        <small>Last Login : <?= $this->session->userdata('last_login') ?></small><br>
+                    </p>
+                </li>
+                <!-- Menu Body -->
 
-                    <!-- Menu Footer-->
-                    <li class="card-body">
-                        <span class="pull-left">
-                            <button class="win7-btn btn-xs" type="button" onclick="window.location.href='<?= base_url('admin/profile'); ?>'"><i class="fa fa-cog"></i>&nbsp;Profile</button>
-                        </span>
-                        <span class="pull-right" style="margin-left: 20%;">
-                            <button class="win7-btn btn-xs" type="button" onclick="window.location.href='<?= base_url('admin/logout'); ?>'"><i class="fas fa-sign-out-alt"></i>&nbsp;Logout</button>
-                        </span>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-    </div>
+                <!-- Menu Footer-->
+                <li class="card-body">
+                    <span class="fa-pull-left">
+                        <button class="btn btn-default btn-sm" type="button" onclick="window.location.href='<?= site_url('admin/profile'); ?>'"><i class="fa fa-cog"></i>&nbsp;Profile</button>
+                    </span>
+                    <span class="fa-pull-right">
+                        <button class="btn btn-default btn-sm" type="button" onclick="window.location.href='<?= site_url('admin/logout'); ?>'"><i class="fas fa-sign-out-alt"></i>&nbsp;Logout</button>
+                    </span>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <!-- <div class="navbar-custom-menu">
+    </div> -->
 </nav>
