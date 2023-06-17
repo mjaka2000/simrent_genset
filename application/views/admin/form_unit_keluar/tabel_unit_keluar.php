5cd345fdfba44fbc8bc1cc18dc968cf99309a2b4
Muhammad Jaka Permana Putra Agus <jakamuhammad28@gmail.com>
2023-06-17 11:51:23 +0800

    Admin view: ubah judul pada form

--- application/views/admin/form_unit_keluar/tabel_unit_keluar.php
+++ application/views/admin/form_unit_keluar/tabel_unit_keluar.php
@@ -8,12 +8,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Data Unit Keluar</h1>
+                    <h1 class="m-0">Data Penyewaan Genset</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="<?= site_url('admin'); ?>"><i class="fas fa-home"></i></a></li>
-                        <li class="breadcrumb-item active">Unit Keluar</li>
+                        <li class="breadcrumb-item active">Penyewaan Genset</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
@@ -32,7 +32,7 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header"><i class="fas fa-table"></i>
-                            Data Unit Keluar
+                            Data Penyewaan Genset
                         </div>
                         <div class="card-body">
                             <?php if ($this->session->flashdata('msg_sukses')) { ?>
@@ -49,6 +49,7 @@
                                         <th style="width :10px">No.</th>
                                         <th>ID</th>
                                         <th>Tanggal Keluar</th>
+                                        <th>Tanggal Masuk (Kembali)</th>
                                         <th>Lokasi</th>
                                         <th>Nama Pelanggan</th>
                                         <th>Nama Genset</th>
@@ -71,6 +72,7 @@
                                                 <td><?= $no++; ?></td>
                                                 <td><?= $dt->id_transaksi; ?></td>
                                                 <td><?= $dt->tanggal_keluar; ?></td>
+                                                <td><?= $dt->tanggal_masuk; ?></td>
                                                 <td><?= $dt->lokasi; ?></td>
                                                 <td><?= $dt->nama_plg; ?></td>
                                                 <td><?= $dt->nama_genset; ?></td>
@@ -101,7 +103,11 @@
                                 </tbody>
                             </table>
                             <div class="box-footer">
-
+                                <?php foreach ($total_data as $td) : ?>
+                                    <h5>
+                                        <p style="color: red;">Total Pendapatan: Rp&nbsp;<?= number_format($td->total); ?></p>
+                                    </h5>
+                                <?php endforeach; ?>
                                 <!-- <h5><strong>Keterangan :</strong></h5> -->
                                 <!-- <a href="#" type="button" class="btn btn-sm btn-info" name="btn_edit"><i class="fa fa-edit mr-2"></i></a>&nbsp;<span>Untuk Mengedit Data</span><br> -->
                                 <!-- <a href="#" type="button" class="btn btn-sm btn-danger" name="btn_delete"><i class="fa fa-trash mr-2"></i></a>&nbsp;<span>Untuk Menghapus Data</span><br> -->
