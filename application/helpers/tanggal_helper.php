2de815885a14b440096b353c4d3886773a559866
Muhammad Jaka Permana Putra Agus <jakamuhammad28@gmail.com>
2023-06-22 13:04:13 +0800

    Memperbaiki helper tanggal

--- application/helpers/tanggal_helper.php
+++ application/helpers/tanggal_helper.php
@@ -6,15 +6,15 @@ if (!function_exists('format_indo')) {
     {
         date_default_timezone_set('Asia/Makassar');
         // array hari dan bulan
-        // $Hari = array("Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu");
+        // $Hari = array("Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu");
         $Bulan = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
 
         // pemisahan tahun, bulan, hari, dan waktu
         $tahun = substr($date, 0, 4);
         $bulan = substr($date, 5, 2);
         $tgl = substr($date, 8, 2);
-        // $waktu = substr($date, 11, 5);
-        $hari = date("w", strtotime($date));
+        // $waktu = substr($date,11,5);
+        // $hari = date("w",strtotime($date));
         $result =  $tgl . " " . $Bulan[(int)$bulan - 1] . " " . $tahun;
 
         return $result;
