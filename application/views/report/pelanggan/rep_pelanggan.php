11b4831d9dd052a98642f3db08145bc5f089af24
Muhammad Jaka Permana Putra Agus <jakamuhammad28@gmail.com>
2023-06-30 12:33:55 +0800

    Report: perbaikan kecil pada view

--- application/views/report/pelanggan/rep_pelanggan.php
+++ application/views/report/pelanggan/rep_pelanggan.php
@@ -47,7 +47,11 @@
                             <td><?= $dt->nama_plg; ?></td>
                             <td><?= $dt->alamat_plg; ?></td>
                             <td><?= $dt->nohp_plg; ?></td>
-                            <td><?= $dt->jk_plg; ?></td>
+                            <?php if ($dt->jk_plg == 'L') { ?>
+                                <td>Laki - Laki</td>
+                            <?php } else { ?>
+                                <td>Perempuan</td>
+                            <?php } ?>
                             <td><?= $dt->namaperusahaan_plg; ?></td>
                             <td><?= date('d-m-Y', strtotime($dt->tglupdate_plg)); ?></td>
 
