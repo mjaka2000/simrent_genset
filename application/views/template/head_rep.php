727b3ccba56b3a2206a47f8506493285e584ce69
Muhammad Jaka Permana Putra Agus <jakamuhammad28@gmail.com>
2023-06-30 13:37:07 +0800

    Perbaikan head report

--- application/views/template/head_rep.php
+++ application/views/template/head_rep.php
@@ -10,48 +10,48 @@
   <link rel="stylesheet" href="<?= base_url() ?>assets/style/style.css">
   <link rel="stylesheet" href="<?= base_url() ?>assets/style/style-rep.css">
 
-  <!-- <style>
-    table {
+  <style>
+    /* table {
       border-collapse: collapse;
       width: 50%;
       margin-top: 10px;
       border: 1px solid #000000;
-    }
+    } */
 
-    .table th {
+    /* .table th {
       padding: 8px 8px;
-      /* border: 1px solid #000000; */
+      border: 1px solid #000000;
       text-align: left;
-    }
+    } */
 
-    .table td {
+    /* .table td {
       padding: 3px 3px;
       border-bottom: 1px solid #000000;
-    }
+    } */
 
 
     @page {
       size: A4
     }
 
-    h4 {
+    /* h4 {
       font-weight: bold;
       font-size: 13pt;
       text-align: center;
-    }
+    } */
 
-    .text-center {
+    /* .text-center {
       text-align: center;
-    }
+    } */
 
-    .horizontal_center {
+    /* .horizontal_center {
       border-top: 3px solid black;
       height: 2px;
       line-height: 30px;
-    }
+    } */
 
-    .kanan {
+    /* .kanan {
       float: right;
-    }
-  </style> -->
+    } */
+  </style>
 </head>
