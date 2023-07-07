15b15677d2014133fc1989342713d1cbdaf1c548
Muhammad Jaka Permana Putra Agus <jakamuhammad28@gmail.com>
2023-07-07 20:35:06 +0800

    View: buat title pada nav

--- application/views/pimpinan/template/nav.php
+++ application/views/pimpinan/template/nav.php
@@ -4,7 +4,9 @@
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
-
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="#" class="nav-link" style="color: white;">Aplikasi SIMRent Genset</a>
+        </li>
     </ul>
     <!-- <li class="nav nav-item dropdown">
         <a href="#" class="nav-link" data-toggle="dropdown" style=" color: white;">
