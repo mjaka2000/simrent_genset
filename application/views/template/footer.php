5b8b2337573074a81f674dbc18fa0a76630ccc29
Muhammad Jaka Permana Putra Agus <jakamuhammad28@gmail.com>
2023-07-12 19:08:00 +0800

    fix footer

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,53 +1,49 @@
-<footer class="main-footer">
+<footer class="main-footer text-sm">
 
-    <div class="float-left d-none d-sm-block">
-
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-sm btn-link" data-toggle="modal" data-target="#staticBackdrop">
-            <b>Version</b>
-        </button>
-
-        <!-- Modal -->
-        <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h6 class="modal-title" id="staticBackdropLabel">About Version</h6>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span>&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <table>
-                            <tr>
-                                <td style="padding: 8px 8px;">
-                                    <img src="<?= base_url('assets/style/logo/ws-w.png'); ?>" alt="logo" width="100px" height="100px">
-                                </td>
-                                <td style="padding: 8px 8px;">
-                                    <h6>
-                                        <p><strong>Version</strong> 1.2.0</p>
-                                        This web app uses:
-                                        <ul>
-                                            <li><a href="https://www.codeigniter.com/download" target="_blank"><u>Codeigniter</u></a> Version 3.1.13</li>
-                                            <li>PHP Version 7.4</li>
-                                        </ul>
-                                        <strong>Copyright &copy; 2022-<script type="text/javascript">
-                                                document.write(new Date().getFullYear());
-                                            </script>
-                                        </strong>
-                                        <!-- Muhammad Jaka Permana Putra Agus -->
-                                    </h6>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Close</button>
-                    </div>
+    <button type="button" class="btn btn-link" data-toggle="modal" data-target="#staticVersion">
+        <b>Version</b>
+    </button>
+    <div class="modal fade" id="staticVersion" data-backdrop="static" data-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h6 class="modal-title" id="staticBackdropLabel">About Version</h6>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span>&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <table>
+                        <tr>
+                            <td style="padding: 8px 8px;">
+                                <img src="<?= base_url('assets/style/logo/ws-w.png'); ?>" alt="logo" width="100px" height="100px">
+                            </td>
+                            <td style="padding: 8px 8px;">
+                                <h6>
+                                    <p><strong>Version</strong> 1.2.0</p>
+                                    This web app uses:
+                                    <ul>
+                                        <li><a href="https://www.codeigniter.com/download" target="_blank"><u>Codeigniter</u></a> Version 3.1.13</li>
+                                        <li>PHP Version 7.4</li>
+                                    </ul>
+                                    <strong>Copyright &copy; 2022-<script type="text/javascript">
+                                            document.write(new Date().getFullYear());
+                                        </script>
+                                    </strong>
+                                    <!-- Muhammad Jaka Permana Putra Agus -->
+                                </h6>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
     </div>
+    <!-- <div class="float-left d-none d-sm-block">
+    </div> -->
 </footer>
 
 
