5efcad660c3e7efb2f838a584bd6b2403e54b312
Muhammad Jaka Permana Putra Agus <jakamuhammad28@gmail.com>
2023-07-20 20:17:57 +0800

    fix controller

--- application/controllers/Pimpinan.php
+++ application/controllers/Pimpinan.php
@@ -27,6 +27,7 @@ class Pimpinan extends CI_Controller
         $data['pendapatan'] = $this->M_data->sum_pendapatanMasuk('tb_pendapatan', $bulan, $tahun);
         $data['numOut'] = $this->M_data->notif_u_keluarJml('tb_unit_penyewaan', $tgl);
         $data['notifOut'] = $this->M_data->notif_u_keluar('tb_unit_penyewaan', $tgl);
+        $data['UnitKeluar'] = $this->M_data->numrows_where_uKeluar('tb_unit_penyewaan');
         $data['stokBarangKeluar'] = $this->M_data->numrows_where_uMasuk('tb_unit_penyewaan');
         $data['dataUser'] = $this->M_data->numrows('tb_user');
         $data['dataPelanggan'] = $this->M_data->numrows('tb_pelanggan');
