e9d080e51b899cfd5a7424b7fbeeda60d7ddf697
Muhammad Jaka Permana Putra Agus <jakamuhammad28@gmail.com>
2023-07-23 10:29:17 +0800

    upload db

--- assets/backup_db/db_rent_genset.sql
+++ assets/backup_db/db_rent_genset.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 21 Jul 2023 pada 05.38
+-- Waktu pembuatan: 23 Jul 2023 pada 04.06
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 7.4.33
 
